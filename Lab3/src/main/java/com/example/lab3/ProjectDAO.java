c28236519959281da11c34803ed583d6956a3ac5
Andrie Tablan <andreitablan01@gmail.com>
2023-10-23 22:30:52 +0300

    Compulsory correct and done

--- Lab3/src/main/java/com/example/lab3/ProjectDAO.java
+++ Lab3/src/main/java/com/example/lab3/ProjectDAO.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class ProjectDAO {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "razvan08";
 
     public static List<Project> getAllProjects() {
         List<Project> projects = new ArrayList<>();
 
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-            String sql = "SELECT * FROM project";
+            String sql = "SELECT * FROM projects";
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery();
 
