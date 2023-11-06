c3ffc03c648c3d374c7a57607c8c902e45f887c7
Andrie Tablan <andreitablan01@gmail.com>
2023-11-06 23:20:36 +0200

    Create ProjectRepositoryTest.java

---
+++ Lab3/src/main/java/com/example/lab3/tests/ProjectRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.example.lab3.tests;
+
+import com.example.lab3.entity.Project;
+import com.example.lab3.repository.ProjectRepository;
+
+public class ProjectRepositoryTest {
+
+    private ProjectRepository projectRepository;
+
+    public void setup() {
+        projectRepository = new ProjectRepository();
+    }
+
+}
